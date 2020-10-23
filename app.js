6825f090042c25e29eaf154fb6f86d67eb65dc76
NathanNOSudo <nathandavid719@gmail.com>
2020-10-23 03:22:53 -0500

    add salt/hash, dev env, dummy users db

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 var createError = require('http-errors');
 var express = require('express');
+var hash = require('pbkdf2-password')();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const { delete } = require('./routes/index');
+// const { delete } = require('./routes/index');
 const { resolveSoa } = require('dns');
 
 var app = express();
-
-//set up a dummy database of users
-var users = {
-  weezy: { name: 'tj' }
-};
 
 // view engine setup and middleware
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// setup local server in development and specify which start 
+// script we are using, in this case dev...
+// see package.json start scripts for refference.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,6 +43,22 @@ app.use(function(req, res, next){
   next();
 })
 
+//set up a dummy database of users
+var users = {
+  weezy: { name: 'tj' }
+};
+
+//When a user is created, generate a salt
+//and hash the passwd ('iRstrong' is the passwd here)
+
+hash({passwd: 'iRstrong'}, function (err, pass, salt, hash){
+  if (err) throw err;
+  //store the salt and hash in the "db"
+  users.weezy.salt = salt;
+  users.weezy.hash = hash;
+});
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -63,5 +80,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// set up a if statement to console log that development
+// server is up and running on specified port
+if (app.use(logger('dev'))) {
+  // app.listen(3000);
+  console.log('Development server started successfully and is running on port 3000')
+}
 
 module.exports = app;
