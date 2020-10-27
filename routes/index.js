e395e08261fa084cd364357c56c268486cbd4bc4
NathanNOSudo <nathandavid719@gmail.com>
2020-10-27 04:46:51 -0500

    converting login .mustache to .hbs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', login: 'please login or register to start tracking'});
 });
 
 module.exports = router;
